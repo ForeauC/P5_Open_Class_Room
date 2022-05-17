de3a7e30064bc87664fd192b2bdc278d1157eb40
ForeauC <clement.foreau@gmail.com>
2022-05-17 22:51:31 +0400

    refactor : change position var

--- front/JS/index.js
+++ front/JS/index.js
@@ -11,7 +11,6 @@ async function addProducts() {
 addProducts()
     .then(function(products){
         
-
         //Pour chaque élément de la réponse, créer un log dans la console et créer le produit dans le DOM (index.html)
         products.forEach(function(element) {
         console.log(element)
@@ -20,28 +19,29 @@ addProducts()
         const newA = document.createElement('a')
         const elementItems = document.getElementById('items')
         newA.href = ('./product.html?id=') + element._id
-        elementItems.appendChild(newA)
 
         //Crée une balise article pour chaque produit
         const newArticle = document.createElement('article')
-        newA.appendChild(newArticle)
 
         //Crée une balise img pour chaque produit
         const newImg = document.createElement('img')
         newImg.src = element.imageUrl
         newImg.alt = element.alTxt
-        newArticle.appendChild(newImg)
 
         //Crée une balise h3 pour chaque produit
         const newH3 = document.createElement('h3')
         newH3.className = ('productName')
         newH3.innerText = element.name
-        newArticle.appendChild(newH3)
 
         //Crée une balise p pour chaque produit
         const newP = document.createElement('p')
         newP.classList = ('productsDescription')
         newP.innerText = element.description 
+
+        elementItems.appendChild(newA)
+        newA.appendChild(newArticle)
+        newArticle.appendChild(newImg)
+        newArticle.appendChild(newH3)
         newArticle.appendChild(newP)
 
         });

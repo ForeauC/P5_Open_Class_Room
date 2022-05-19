c3a746c5e6d44a3ebde696e05dc42d014f2ee915
ForeauC <clement.foreau@gmail.com>
2022-05-19 23:01:48 +0400

    fix : improves the js code index.js

--- front/JS/index.js
+++ front/JS/index.js
@@ -1,50 +1,60 @@
-// La fonction addProduct permet de récupère tous les produits par l'API 
-async function addProducts() {
-    const response = await fetch('http://localhost:3000/api/products')
-    if(response.ok){
-        return response.json()
-    }
+let products = [];
+
+async function init() {
+    products = await getProducts(); 
+    console.log(products);
+    createHTML(products);
+}    
+
+function getProducts() {
+    return ( 
+    fetch('http://localhost:3000/api/products')
+        .then((res) => res.json())
+        .then((data) => {
+            console.log("data", data);
+            return data
+        })
+        .catch((data) => { 
+            return error;
+        }) 
+    );
 }
 
-// Appeler la fonction addProduct
-
-addProducts()
-    .then(function(products){
-        
-        //Pour chaque élément de la réponse, créer un log dans la console et créer le produit dans le DOM (index.html)
-        products.forEach(function(element) {
-        console.log(element)
+function createHTML(products){
+    
+    products.forEach(function(element){
+        console.log(element);
     
         //Crée une balise a pour chaque produit
-        const newA = document.createElement('a')
-        const elementItems = document.getElementById('items')
-        newA.href = ('./product.html?id=') + element._id
+        const newA = document.createElement('a');
+        const elementItems = document.getElementById('items');
+        newA.href = ('./product.html?id=') + element._id;
 
         //Crée une balise article pour chaque produit
-        const newArticle = document.createElement('article')
+        const newArticle = document.createElement('article');
 
         //Crée une balise img pour chaque produit
-        const newImg = document.createElement('img')
-        newImg.src = element.imageUrl
-        newImg.alt = element.alTxt
+        const newImg = document.createElement('img');
+        newImg.src = element.imageUrl;
+        newImg.alt = element.altTxt;
 
         //Crée une balise h3 pour chaque produit
-        const newH3 = document.createElement('h3')
-        newH3.className = ('productName')
-        newH3.innerText = element.name
+        const newH3 = document.createElement('h3');
+        newH3.className = ('productName');
+        newH3.innerText = element.name;
 
         //Crée une balise p pour chaque produit
-        const newP = document.createElement('p')
-        newP.classList = ('productsDescription')
-        newP.innerText = element.description 
+        const newP = document.createElement('p');
+        newP.classList = ('productsDescription');
+        newP.innerText = element.description;
 
-        elementItems.appendChild(newA)
-        newA.appendChild(newArticle)
-        newArticle.appendChild(newImg)
-        newArticle.appendChild(newH3)
-        newArticle.appendChild(newP)
+        elementItems.appendChild(newA);
+        newA.appendChild(newArticle);
+        newArticle.appendChild(newImg);
+        newArticle.appendChild(newH3);
+        newArticle.appendChild(newP);
 
         });
+}
 
-    })
-
+init();
--- front/JS/product.js
+++ front/JS/product.js
@@ -1,7 +1,44 @@
 //Récupérer la valeur id à partir de l'URL
-let params = new URL(document.location).searchParams;
-let id = params.get("id");// id du produit
-console.log(id);
 
 
+//ajouter un image du produit
+const productImg = document.querySelector('.item__img');
+const imgSofa = document.createElement('img');
+productImg.appendChild(imgSofa);
 
+
+const productDescription = document.getElementById('title');
+
+const productPrice = document.getElementById('price');
+
+const productColor = document.getElementById('color');
+
+// Fonction pour récupérer un produit avec son id
+async function getProductById (){
+    let params = new URL(document.location).searchParams;
+    let id = params.get("id");// id du produit
+    console.log(id);
+    console.log(`http://localhost:3000/api/products/${id}`)
+    const response = await fetch(`http://localhost:3000/api/products/${id}`);
+    if(response.ok){
+        console.log(response)
+        return response.json
+    }     
+}
+
+const product = await getProductById();
+createElements(product);
+
+function createElements(array) {
+    console.log(array)
+    .then(function(product){
+        product.forEach(function(element) {
+            console.log(element);
+        let productImg = document.querySelector(".item__img");
+        let img = document.createElement("img"); // Create img for the product.
+        productImg.appendChild(img);
+
+        })
+    })
+    
+}

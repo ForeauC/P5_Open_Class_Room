f77926667a1a6c2e7aa0129df6a90f43a7d46830
ForeauC <clement.foreau@gmail.com>
2022-05-26 15:54:37 +0400

    fix : change function page product for localstorage

--- front/JS/product.js
+++ front/JS/product.js
@@ -3,8 +3,8 @@ let product = [];
 async function init(){
     product = await getProductById();
     console.log(product)
-    buildHTML(product);
-    addToCard ();
+    buildHTML();
+    clickButtonCard();
 }
 
 //function pour récupérer le produit avec son ID à partir de l'API
@@ -26,45 +26,52 @@ function getProductById() {
 }
 
 function buildHTML() {
-        //ajout de l'image et du alt texte du produit à partir de l'API
-        const itemImg = document.querySelector(".item__img");
-        const productImg = document.createElement("img"); 
-        productImg.src = product.imageUrl;
-        productImg.alt = product.altText;
-        itemImg.appendChild(productImg);
-        //ajout du nom du produit à partir de l'API
-        const productTitle = document.getElementById("title");
-        productTitle.innerHTML = product.name;
-        //ajout du prix du produit à partir de l'API
-        const productPrice = document.getElementById("price");
-        productPrice.innerHTML = product.price;
-        //ajout de la description du produit à partir du produit 
-        const productDescription = document.getElementById("description");
-        productDescription.innerHTML = product.description;
+    //ajout de l'image et du alt texte du produit à partir de l'API
+    const itemImg = document.querySelector(".item__img");
+    const productImg = document.createElement("img"); 
+    productImg.src = product.imageUrl;
+    productImg.alt = product.altText;
+    itemImg.appendChild(productImg);
+    //ajout du nom du produit à partir de l'API
+    const productTitle = document.getElementById("title");
+    productTitle.innerHTML = product.name;
+    //ajout du prix du produit à partir de l'API
+    const productPrice = document.getElementById("price");
+    productPrice.innerHTML = product.price;
+    //ajout de la description du produit à partir du produit 
+    const productDescription = document.getElementById("description");
+    productDescription.innerHTML = product.description;
         
-        const productColorsChoice = document.getElementById("colors");
-        //ajout du choix du couleur avec une boucle for à partir de l'API
-        for (let i = 0; i < product.colors.length; i++){
-            const colorChoice = document.createElement("option");
-            colorChoice.value = product.colors[i];
-            colorChoice.innerHTML = product.colors[i];
-            productColorsChoice.appendChild(colorChoice);
-        }     
+    const productColorsChoice = document.getElementById("colors");
+    //ajout du choix du couleur avec une boucle for à partir de l'API
+    for (let i = 0; i < product.colors.length; i++){
+        const colorChoice = document.createElement("option");
+        colorChoice.value = product.colors[i];
+        colorChoice.innerHTML = product.colors[i];
+        productColorsChoice.appendChild(colorChoice);
+    }     
 }      
 
-function addToCard () {
-    const colorsOption = document.getElementById("colors");
-    const productQuantity = document.getElementById("quantity");    
-    const addToCartButton = document.getElementById("addToCart");
-    //Click sur le bouton "ajouter panier" 
+const colorsOption = document.getElementById("colors");
+const productQuantity = document.getElementById("quantity");    
+const addToCartButton = document.getElementById("addToCart");
+
+const color = colorsOption.value;
+const quantity = productQuantity.value;
+
+function clickButtonCard() {
     addToCartButton.addEventListener("click", (event) => {
-        const color = colorsOption.value;
-        const quantity = productQuantity.value;
+        addToBasket();
+    })
+}
+
+function addToBasket() {
+    const color = colorsOption.value;
+    const quantity = productQuantity.value;
         //si au moin une de ces valeurs n'est pas acceptée (ajout au panier invalide)
         if (color == "" || quantity < 1 || quantity > 100) {
             alert("Renseignez la couleur") = color;
             alert("Rensignez la bonne quantité") = quantity;
-        //sinon valider le produit avec couleur / quantité / ID du produit
         } else {
             const validProduct = {
                 color: color,
@@ -73,26 +80,38 @@ function addToCard () {
             };
             alert("produit ajouté au panier avec succès")
             console.log(validProduct);
-        }// si il y a déja un produit du même modéle et même couleurs dans localStorage
-        if (localStorage.getItem("product")) {
-            const productInLocalStorage = JSON.parse(localStorage.getItem("product"));
-            const searchProduct = productInLocalStorage.find(product => product.color === color && product.idProduct == id);
-            //ajouter seulement une nouvelle quantité
-            if (searchProduct){
-                const newQuantity = parseInt(quantity) + parseInt(searchProduct.quantity);
-                searchProduct = newQuantity;
-                localStorage.setItem("product", JSON.stringify(productInLocalStorage));
-            }else{ //sinon ajouter le produit dans localStorage
-                const productInLocalStorage = JSON.parse(localStorage.getItem("product"));
-                productInLocalStorage.push(validProduct);
-                localStorage.setItem("product", JSON.stringify(productInLocalStorage));
-                console.log(productInLocalStorage)
-            }
+            addCart(product);
         }
-        
-    })
-        
-}             
+}
+
+//Function qui permet d'enregister le panier dans le local storage
+function saveCart(cart) {
+    localStorage.setItem("cart", JSON.stringify(cart));
+}
+    
+// Function qui permet de récuper l'item "cart"
+function getCart() {
+    let cart = (localStorage.getItem("cart"));
+    if(cart == null){
+        return [];
+    }else{
+        return JSON.parse(cart);
+    }      
+}
+
+// function pour ajouter au panier
+function addCart(product) {
+    let cart = getCart();
+    let foundProduct = cart.find(p => p.id == product.id);
+    if(foundProduct != undefined){
+        foundProduct.quantity++;
+    }else{
+        product.quantity = 1;
+        cart.push(product)
+    }
+    saveCart(cart);
+}
+
 
 init();
 
